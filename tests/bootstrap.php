d18f4dd9a4cfe8cc50d92b39d6f841f2b22c9e9c
Trismegiste <florent.g@allopneus.com>
2013-06-09 13:50:02 +0200

    for future dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@
 if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
             'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
-            'php composer.phar install' . PHP_EOL);
+            'php composer.phar install --dev' . PHP_EOL);
 }
 
 $loader->add('Tests', __DIR__);
